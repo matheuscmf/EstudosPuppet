ace688023f0f4e7cc8ff6d1dfcacd70be953b11b
Matheus Marques Albuquerque <80928621+matheuscmf@users.noreply.github.com>
2022-10-08 03:48:19 -0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,8 +5,11 @@ node default {
     owner   => 'root', 
     }
     }
-node 'master.puppet.vm' {
+node master.puppet.vm {
     include role:: master_server
+    file { '/root/README':
+    ensure  => file,
+    content => "Welcome to ${fqdn}"\n ,
 }
 
 node /^web/ {
