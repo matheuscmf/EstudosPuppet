de68cc47bfb521bd1c9fb255583b615467830bca
Matheus Marques Albuquerque <80928621+matheuscmf@users.noreply.github.com>
2022-10-08 03:02:28 -0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,17 @@
 node default {
-   # file {'/root/README' :
-   # ensure  => file ,
-   # content => 'Esse é um README',
-   # owner   => 'root', 
+    file {'/root/README' :
+    ensure  => file ,
+    content => 'Esse é um README',
+    owner   => 'root', 
+    }
     }
 node 'master.puppet.vm' {
     include role:: master_server
+}
+
+node /^web/ {
+   include role::app_server
+}
+node /^db/ {
+   include role::db_server
 }
